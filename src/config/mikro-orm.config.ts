6519c70bea674f2c122c2d2ef799e4f2c7d1225f
Joel Arnold <joel.arnold@ecloudsolutions.com>
2025-08-04 16:57:00 -0300

    Cambiar datos de conexión a la base de datos

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -4,11 +4,11 @@ import { User } from '../entities/User';
 
 const config: Options<MySqlDriver> = {
   driver: MySqlDriver,
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'root',
-  dbName: 'api-express-demo',
+  host: 'base-de-datos-mysql-api-express-demo.d.aivencloud.com',
+  port: 26759,
+  user: 'avnadmin',
+  password: 'AVNS_39ylc3ReuR39PNfdInN',
+  dbName: 'defaultdb',
   charset: 'utf8mb4',
   debug: process.env.NODE_ENV !== 'production',
   allowGlobalContext: true,
